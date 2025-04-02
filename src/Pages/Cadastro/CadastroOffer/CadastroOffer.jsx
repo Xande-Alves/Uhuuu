a2b95773dd88fd9fbd6b89923d1af919dad33b50
Xande-Alves <alexandre.upe@gmail.com>
2025-04-02 14:19:33 -0300

    Integração da API para cadastro, resp. 500px

--- src/Pages/Cadastro/CadastroOffer/CadastroOffer.jsx
+++ src/Pages/Cadastro/CadastroOffer/CadastroOffer.jsx
@@ -1,8 +1,105 @@
 import Footer from "../../../Components/Footer/Footer";
 import Header from "../../../Components/Header/Header";
 import s from "./cadastroOffer.module.scss";
+import { useState } from "react";
+import axios from "axios";
 
 export default function CadastroOffer() {
+  const [nome, setNome] = useState("");
+  const [logradouro, setLogradouro] = useState("");
+  const [numero, setNumero] = useState("");
+  const [complemento, setComplemento] = useState("");
+  const [bairro, setBairro] = useState("");
+  const [cidade, setCidade] = useState("");
+  const [estado, setEstado] = useState("");
+  const [email, setEmail] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confirmaSenha, setConfirmaSenha] = useState("");
+
+  const capturaNome = (e) => {
+    setNome(e.target.value);
+  };
+  const capturaLogradouro = (e) => {
+    setLogradouro(e.target.value);
+  };
+  const capturaNumero = (e) => {
+    setNumero(e.target.value);
+  };
+  const capturaComplemento = (e) => {
+    setComplemento(e.target.value);
+  };
+  const capturaBairro = (e) => {
+    setBairro(e.target.value);
+  };
+  const capturaCidade = (e) => {
+    setCidade(e.target.value);
+  };
+  const capturaEstado = (e) => {
+    setEstado(e.target.value);
+  };
+  const capturaEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const capturaTelefone = (e) => {
+    setTelefone(e.target.value);
+  };
+  const capturaSenha = (e) => {
+    setSenha(e.target.value);
+  };
+  const capturaConfirmaSenha = (e) => {
+    setConfirmaSenha(e.target.value);
+  };
+
+  const enviarDados = async (e) => {
+    e.preventDefault();
+
+    if (senha !== confirmaSenha) {
+      alert(
+        "Os campos de senha e confirmação de senha não coincidem. Por favor, tente novamente."
+      );
+      return;
+    }
+
+    const endPointAPI = "https://api-uhuuu.onrender.com/cadastrar_offer";
+
+    const dadosAEnviar = {
+      nome,
+      logradouro,
+      numero,
+      complemento,
+      bairro,
+      cidade,
+      estado,
+      email,
+      telefone,
+      senha,
+    };
+
+    try {
+      const resposta = await axios.post(endPointAPI, dadosAEnviar);
+      alert("Usuário ofertador cadastrado com sucesso!");
+
+      // Limpa os campos após o envio bem-sucedido
+      setNome("");
+      setLogradouro("");
+      setNumero("");
+      setComplemento("");
+      setBairro("");
+      setCidade("");
+      setEstado("");
+      setEmail("");
+      setTelefone("");
+      setSenha("");
+      setConfirmaSenha("");
+    } catch (erro) {
+      console.error("Erro ao cadastrar usuário buscador:", erro);
+      alert(
+        "Erro ao cadastrar usuário ofertador. Verifique os dados e tente novamente."
+      );
+    }
+  };
+
   return (
     <>
       <Header />
@@ -16,20 +113,28 @@ export default function CadastroOffer() {
                 type="text"
                 id="nome"
                 placeholder="Digite seu primeiro nome"
+                value={nome}
+                onChange={capturaNome}
                 required
               />
             </div>
             <div className={s.endereco}>
-              <label htmlFor="endereço">Endereço</label>
+              <label className={s.labelEndereco} htmlFor="endereço">
+                Endereço
+              </label>
               <div className={s.dadosEndereco}>
                 <div className={s.rua}>
                   <div>
-                    <label htmlFor="logradouro">Logradouro</label>
+                    <label className={s.labelLogradouro} htmlFor="logradouro">
+                      Logradouro
+                    </label>
                     <input
                       className={s.inputLogradouro}
                       type="text"
                       id="logradouro"
                       placeholder="Digite sua rua, avenida..."
+                      value={logradouro}
+                      onChange={capturaLogradouro}
                       required
                     />
                   </div>
@@ -41,6 +146,8 @@ export default function CadastroOffer() {
                       className={s.inputNumero}
                       type="number"
                       id="numero"
+                      value={numero}
+                      onChange={capturaNumero}
                       required
                     />
                   </div>
@@ -50,6 +157,8 @@ export default function CadastroOffer() {
                       type="text"
                       id="complemento"
                       placeholder="Complemento do endereço"
+                      value={complemento}
+                      onChange={capturaComplemento}
                     />
                   </div>
                 </div>
@@ -60,6 +169,8 @@ export default function CadastroOffer() {
                       type="text"
                       id="bairro"
                       placeholder="Digite seu bairro"
+                      value={bairro}
+                      onChange={capturaBairro}
                       required
                     />
                   </div>
@@ -69,12 +180,20 @@ export default function CadastroOffer() {
                       type="text"
                       id="cidade"
                       placeholder="Digite a cidade"
+                      value={cidade}
+                      onChange={capturaCidade}
                       required
                     />
                   </div>
                   <div className={s.divEstado}>
                     <label htmlFor="estado">Estado</label>
-                    <input type="text" id="estado" required />
+                    <input
+                      type="text"
+                      id="estado"
+                      value={estado}
+                      onChange={capturaEstado}
+                      required
+                    />
                   </div>
                 </div>
               </div>
@@ -85,6 +204,8 @@ export default function CadastroOffer() {
                 type="email"
                 id="email"
                 placeholder="Digite seu e-mail"
+                value={email}
+                onChange={capturaEmail}
                 required
               />
             </div>
@@ -94,6 +215,8 @@ export default function CadastroOffer() {
                 type="tel"
                 id="telefone"
                 placeholder="Digite seu telefone"
+                value={telefone}
+                onChange={capturaTelefone}
                 required
               />
             </div>
@@ -103,20 +226,26 @@ export default function CadastroOffer() {
                 type="password"
                 id="password"
                 placeholder="Digite sua senha"
+                value={senha}
+                onChange={capturaSenha}
                 required
               />
             </div>
-            <div>
+            <div className={s.divConfirmaSenha}>
               <label htmlFor="passwordConfirma">Confirme sua senha</label>
               <input
                 type="password"
                 id="passwordConfirma"
                 placeholder="Confirme sua senha"
+                value={confirmaSenha}
+                onChange={capturaConfirmaSenha}
                 required
               />
             </div>
             <div className={s.cadastroDivButtom}>
-              <button type="submit">Cadastrar</button>
+              <button type="submit" onClick={enviarDados}>
+                Cadastrar
+              </button>
             </div>
           </form>
         </section>
--- src/Pages/Cadastro/CadastroSeacher/CadastroSeacher.jsx
+++ src/Pages/Cadastro/CadastroSeacher/CadastroSeacher.jsx
@@ -1,8 +1,81 @@
 import Footer from "../../../Components/Footer/Footer";
 import Header from "../../../Components/Header/Header";
 import s from "./cadastroSeacher.module.scss";
+import axios from "axios";
+import { useState } from "react";
 
 export default function CadastroSeacher() {
+  const [nome, setNome] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
+  const [data_nascimento, setData_nascimento] = useState("");
+  const [email, setEmail] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confirmaSenha, setConfirmaSenha] = useState("");
+
+  const capturaNome = (e) => {
+    setNome(e.target.value);
+  };
+  const capturaSobrenome = (e) => {
+    setSobrenome(e.target.value);
+  };
+  const capturaDataNascimento = (e) => {
+    setData_nascimento(e.target.value);
+  };
+  const capturaEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const capturaTelefone = (e) => {
+    setTelefone(e.target.value);
+  };
+  const capturaSenha = (e) => {
+    setSenha(e.target.value);
+  };
+  const capturaConfirmaSenha = (e) => {
+    setConfirmaSenha(e.target.value);
+  };
+
+  const enviarDados = async (e) => {
+    e.preventDefault();
+
+    if (senha !== confirmaSenha) {
+      alert(
+        "Os campos de senha e confirmação de senha não coincidem. Por favor, tente novamente."
+      );
+      return;
+    }
+
+    const endPointAPI = "https://api-uhuuu.onrender.com/cadastrar_seacher";
+
+    const dadosAEnviar = {
+      nome,
+      sobrenome,
+      data_nascimento,
+      email,
+      telefone,
+      senha,
+    };
+
+    try {
+      const resposta = await axios.post(endPointAPI, dadosAEnviar);
+      alert("Usuário buscador cadastrado com sucesso!");
+
+      // Limpa os campos após o envio bem-sucedido
+      setNome("");
+      setSobrenome("");
+      setData_nascimento("");
+      setEmail("");
+      setTelefone("");
+      setSenha("");
+      setConfirmaSenha("");
+    } catch (erro) {
+      console.error("Erro ao cadastrar usuário buscador:", erro);
+      alert(
+        "Erro ao cadastrar usuário buscador. Verifique os dados e tente novamente."
+      );
+    }
+  };
+
   return (
     <>
       <Header />
@@ -18,6 +91,8 @@ export default function CadastroSeacher() {
                 type="text"
                 id="nome"
                 placeholder="Digite seu primeiro nome"
+                value={nome}
+                onChange={capturaNome}
                 required
               />
             </div>
@@ -29,6 +104,8 @@ export default function CadastroSeacher() {
                 type="text"
                 id="sobrenome"
                 placeholder="Digite seu sobrenome"
+                value={sobrenome}
+                onChange={capturaSobrenome}
                 required
               />
             </div>
@@ -40,6 +117,8 @@ export default function CadastroSeacher() {
                 type="date"
                 id="dataNascimento"
                 placeholder="Digite sua data de nascimento"
+                value={data_nascimento}
+                onChange={capturaDataNascimento}
                 required
               />
             </div>
@@ -51,6 +130,8 @@ export default function CadastroSeacher() {
                 type="email"
                 id="email"
                 placeholder="Digite seu e-mail"
+                value={email}
+                onChange={capturaEmail}
                 required
               />
             </div>
@@ -62,6 +143,8 @@ export default function CadastroSeacher() {
                 type="tel"
                 id="telefone"
                 placeholder="Digite seu telefone"
+                value={telefone}
+                onChange={capturaTelefone}
                 required
               />
             </div>
@@ -73,6 +156,8 @@ export default function CadastroSeacher() {
                 type="password"
                 id="password"
                 placeholder="Digite sua senha"
+                value={senha}
+                onChange={capturaSenha}
                 required
               />
             </div>
@@ -84,11 +169,15 @@ export default function CadastroSeacher() {
                 type="password"
                 id="passwordConfirma"
                 placeholder="Confirme sua senha"
+                value={confirmaSenha}
+                onChange={capturaConfirmaSenha}
                 required
               />
             </div>
             <div className={s.cadastroDivButtom}>
-              <button type="submit">Cadastrar</button>
+              <button type="submit" onClick={enviarDados}>
+                Cadastrar
+              </button>
             </div>
           </form>
         </section>
